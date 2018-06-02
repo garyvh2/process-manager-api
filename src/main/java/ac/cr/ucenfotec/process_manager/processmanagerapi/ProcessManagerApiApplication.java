daa03dd80654629f8d50215a6cc1826569572cb4
Gary Valverde Hampton <garyhampton.12@gmail.com>
2018-06-01 22:19:14 -0600

    Initial Commit

---
+++ src/main/java/ac/cr/ucenfotec/process_manager/processmanagerapi/ProcessManagerApiApplication.java
@@ -0,0 +1,12 @@
+package ac.cr.ucenfotec.process_manager.processmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessManagerApiApplication.class, args);
+	}
+}
