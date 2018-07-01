d09a054485fa672fbf1554e35cfdf700c819d65b
JoseSolano <jsolanom>
2018-06-30 18:38:00 -0600

    Refactoring and Implementing Equals

--- src/main/java/ac/cr/ucenfotec/process_manager/entities/Process.java
+++ src/main/java/ac/cr/ucenfotec/process_manager/entities/Process.java
@@ -7,46 +7,55 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
 @Document(collection ="processes")
 public class Process {
 	
 	@Id
 	String numeroTramite;
+	
 	@NotBlank @NotNull @NotEmpty
 	private String description;
+	
 	@NotNull
 	private ArrayList<Task> tasks;
 	private User requester;
 	
-
 	public String getNumeroTramite() {
 		return numeroTramite;
 	}
+	
 	public void setNumeroTramite(String numeroTramite) {
 		this.numeroTramite = numeroTramite;
 	}
+	
 	public String getDescription() {
 		return description;
 	}
+	
 	public void setDescription(String descripcion) {
 		description = descripcion;
 	}
+	
 	public ArrayList<Task> getTasks() {
 		return tasks;
 	}
+	
 	public void setTasks(ArrayList<Task> tareas) {
 		tasks = tareas;
 	}
+	
 	public void setTask(Task tarea) {
 		if(tasks == null )
 			tasks = new ArrayList<Task>();
 		
-		tasks.add(tarea);
-		
+		tasks.add(tarea);		
 	}
+	
 	public User getRequester() {
 		return requester;
 	}
+	
 	public void setRequester(User requester) {
 		this.requester = requester;
 	}
@@ -71,7 +80,6 @@ public class Process {
 		} else if (!numeroTramite.equals(other.numeroTramite))
 			return false;
 		return true;
-	}
-	
+	}	
 	
 }
